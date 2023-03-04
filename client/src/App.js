8b03db326dffb058f3b59a51705afe24272d0499
TilakSojitra <pateltilak9723@gmail.com>
2023-03-04 15:28:24 +0530

    04-03

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import {React} from "react";
-import { Routes,Route,BrowserRouter } from "react-router-dom";
+import { React } from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 import ForgetPassword from "./Components/ForgetPassword";
 
@@ -11,32 +11,35 @@ import EditUser from "./Components/User/EditUser";
 import PaymentInfo from "./Components/User/PaymentInfo";
 import NgoHome from "./Components/NGO/home";
 import NgoProfile from "./Components/NGO/Profile/profile";
-import UserProfile from "./Components/User/Profile/profile";
 import NgoLogin from "./Components/NGO/Ngo_Login/NgoLogin";
-import UserLogin from "./Components/User/User_Login/UserLogin";
+import UserLogin from "./Components/User/UserLogin";
+import { UserContextProvider } from './Context/UserContext';
+
 const App = () => {
- 
+
+
+
   return (
     <>
-
-      <BrowserRouter>
-        <Routes>
-          <Route exact path ='/user/login' element={<UserLogin/>} />
-          <Route exact path ='/user/signup' element={<UserSignup/>} />
-          <Route exact path ='/user/profile' element={<ViewProfile/>} />
-          <Route exact path ='/user/payment' element={<PaymentInfo/>} />
-          <Route exact path ='/user/edit' element={<EditUser />} />
-          <Route exact path ='/ngo/signup' element={<NgoSignup/>} />
-          <Route exact path ='/forgetpassword' element={<ForgetPassword/>} />
-          <Route exact path ='/ngo/signup' element={<NgoSignup/>} /> 
-          <Route exact path ='/ngo/login' element={<NgoLogin/>} />
-          <Route exact path ='/ngo/home' element={<NgoHome/>} />
-          <Route exact path ='/ngo/profile' element={<NgoProfile/>} />
-          <Route exact path ='/user/profile' element={<UserProfile/>} />
-          <Route exact path ='/' element={<Home/>} />
-          <Route exact path ='/forgetpassword' element={<ForgetPassword/>} />
-        </Routes>
-      </BrowserRouter>
+      <UserContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path='/user/login' element={<UserLogin />} />
+            <Route exact path='/user/signup' element={<UserSignup />} />
+            <Route exact path='/user/profile' element={<ViewProfile />} />
+            <Route exact path='/user/payment' element={<PaymentInfo />} />
+            <Route exact path='/user/edit' element={<EditUser />} />
+            <Route exact path='/ngo/signup' element={<NgoSignup />} />
+            <Route exact path='/forgetpassword' element={<ForgetPassword />} />
+            <Route exact path='/ngo/signup' element={<NgoSignup />} />
+            <Route exact path='/ngo/login' element={<NgoLogin />} />
+            <Route exact path='/ngo/home' element={<NgoHome />} />
+            <Route exact path='/ngo/profile' element={<NgoProfile />} />
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/forgetpassword' element={<ForgetPassword />} />
+          </Routes>
+        </BrowserRouter>
+      </UserContextProvider>
     </>
   );
 };
